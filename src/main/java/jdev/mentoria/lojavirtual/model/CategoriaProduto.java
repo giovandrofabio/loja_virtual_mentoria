1a13a87b1909e75d127c7c614e8d6062c4d96481
giovandrofabio <giovandrofabiosantos@hotmail.com>
2023-07-23 19:25:22 -0300

    criado classe de categoria produto

--- src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/CategoriaProduto.java
@@ -5,17 +5,17 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table(name="marca_produto")
-@SequenceGenerator(name="seq_marca_produto", sequenceName ="seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name="categoria_produto")
+@SequenceGenerator(name="seq_categoria_produto", sequenceName ="seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name="nome_desc", nullable = false)
     private String nomeDesc;
 
     public Long getId() {
@@ -37,13 +37,13 @@ public class MarcaProduto implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof MarcaProduto)) return false;
-        MarcaProduto that = (MarcaProduto) o;
-        return getId().equals(that.getId()) && Objects.equals(getNomeDesc(), that.getNomeDesc());
+        if (!(o instanceof CategoriaProduto)) return false;
+        CategoriaProduto that = (CategoriaProduto) o;
+        return getId().equals(that.getId());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getNomeDesc());
+        return Objects.hash(getId());
     }
 }
