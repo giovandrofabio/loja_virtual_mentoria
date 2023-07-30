0e274f4bf0f5d9e1b465088aab55c180a8d7f22f
giovandrofabio <giovandrofabiosantos@hotmail.com>
2023-07-29 21:32:40 -0300

    Configurando Flyway

--- src/main/java/jdev/mentoria/lojavirtual/model/Pessoa.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Pessoa.java
@@ -29,6 +29,8 @@ public abstract class Pessoa implements Serializable {
     private String email;
     @Column(nullable = false)
     private String telefone;
+    @Column
+    private String tipoPessoa;
     @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Endereco> enderecos = new ArrayList<Endereco>();
 }
--- src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
+++ src/test/java/jdev/mentoria/lojavirtual/LojaVirtualMentoriaApplicationTests.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +25,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 //@RunWith(SpringRunner.class)
+@Profile("test")
 @WebAppConfiguration
 @SpringBootTest(classes = LojaVirtualMentoriaApplication.class)
 public class LojaVirtualMentoriaApplicationTests extends TestCase {
