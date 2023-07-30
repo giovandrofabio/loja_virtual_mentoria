ca78aa048324bab2f602c660a5cf6c64a0b12f55
giovandrofabio <giovandrofabiosantos@hotmail.com>
2023-07-30 10:30:12 -0300

    Configurando de TOKEN

--- src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
+++ src/main/java/jdev/mentoria/lojavirtual/security/WebConfigSecurity.java
@@ -6,10 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.servlet.http.HttpSessionListener;
 
@@ -21,6 +25,28 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
     @Autowired
     private ImplementacaoUserDetailsService implementacaoUserDetailsService;
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .disable().authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/index").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                /* redireciona ou da um retorno para index quando desloga*/
+                .anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+
+                /*mapeia o logout do sistema*/
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+
+                /*Filtra as requisicoes para login de JWT*/
+                .and().addFilterAfter(new JWTLoginFilter("/login", authenticationManager()),
+                        UsernamePasswordAuthenticationFilter.class)
+
+                .addFilterBefore(new JwtApiAutenticacaoFilter(), UsernamePasswordAuthenticationFilter.class);
+
+    }
 
     /*Irá consultar o user no banco com Spring Security*/
     @Override
@@ -30,8 +56,8 @@ public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements H
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso","/deleteAcesso")
-                      .antMatchers(HttpMethod.POST, "/salvarAcesso","/deleteAcesso");
+        //web.ignoring().antMatchers(HttpMethod.GET, "/salvarAcesso","/deleteAcesso")
+        //.antMatchers(HttpMethod.POST, "/salvarAcesso","/deleteAcesso");
         /*Ingnorando URL no momento para não autenticar*/
     }
 }
