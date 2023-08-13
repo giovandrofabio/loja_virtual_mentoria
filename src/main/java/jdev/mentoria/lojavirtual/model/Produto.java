f9de80b291127ed6b7266114ad172a89499749cb
giovandrofabio <giovandrofabiosantos@hotmail.com>
2023-08-12 21:20:59 -0300

    Cadastro de Produto

--- src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/Produto.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
@@ -23,31 +25,41 @@ public class Produto implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
     private Long id;
 
+    @NotNull(message = "O tipo da unidade deve ser informado")
     @Column(nullable = false)
     private String tipoUnidade;
 
+    @Size(min = 10, message = "Nome do produto deve ter mais de 10 letras")
+    @NotNull(message = "Nome do produto deve ser informado")
     @Column(nullable = false)
     private String nome;
 
     @Column(nullable = false)
     private Boolean ativo = Boolean.TRUE;
 
+    @NotNull(message = "Descrição do produto deve ser informada")
     @Column(columnDefinition = "text", length = 2000, nullable = false)
     private String descricao;
 
     /**Nota item nota produto - ASSOCIAR **/
+
+    @NotNull(message = "Peso deve ser informado")
     @Column(nullable = false)
     private Double peso;
 
+    @NotNull(message = "Largura deve ser informado")
     @Column(nullable = false)
     private Double largura;
 
+    @NotNull(message = "Altura deve ser informado")
     @Column(nullable = false)
     private Double altura;
 
+    @NotNull(message = "Profundidade")
     @Column(nullable = false)
     private Double profundidade;
 
+    @NotNull(message = "Valor de venda deve ser informado")
     @Column(nullable = false)
     private BigDecimal valorVenda = BigDecimal.ZERO;
 
@@ -62,7 +74,18 @@ public class Produto implements Serializable {
 
     private Integer qtdeClique = 0;
 
+    @NotNull(message = "A empresa responsável deve ser informada")
     @ManyToOne(targetEntity = Pessoa.class)
     @JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-    private Pessoa empresa;
+    private PessoaJuridica empresa;
+
+    @NotNull(message = "A Categoria do Produto deve ser informada")
+    @ManyToOne(targetEntity = CategoriaProduto.class)
+    @JoinColumn(name = "categoria_produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "categoria_produto_id_fk"))
+    private CategoriaProduto categoriaProduto = new CategoriaProduto();
+
+    @NotNull(message = "A Marca do Produto deve ser informada")
+    @ManyToOne(targetEntity = MarcaProduto.class)
+    @JoinColumn(name = "marca_produto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "marca_produto_id_fk"))
+    private MarcaProduto marcaProduto = new MarcaProduto();
 }
