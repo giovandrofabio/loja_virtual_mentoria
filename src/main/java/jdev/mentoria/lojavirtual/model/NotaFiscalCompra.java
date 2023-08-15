c2296fe5e996823aae1bd26986ec9b63491ba322
giovandrofabio <giovandrofabiosantos@hotmail.com>
2023-08-14 22:36:36 -0300

    Cadastro de Nota Fiscal

--- src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalCompra.java
+++ src/main/java/jdev/mentoria/lojavirtual/model/NotaFiscalCompra.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
@@ -24,35 +25,40 @@ public class NotaFiscalCompra implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
     private Long id;
 
+    @NotNull(message = "Informe o número da nota")
     @Column(nullable = false)
     private String numeroNota;
 
+    @NotNull(message = "Informe a série da nota")
     @Column(nullable = false)
     private String serieNota;
 
     private String descricaoObs;
 
+    @NotNull(message = "Informe o total da nota")
     @Column(nullable = false)
     private BigDecimal valorTotal;
 
     private BigDecimal valorDesconto;
 
+    @NotNull(message = "Informe o valor do ICMS")
     @Column(nullable = false)
     private BigDecimal valorIcms;
 
+    @NotNull(message = "Informe a data da compra")
     @Column(nullable = false)
     @Temporal(TemporalType.DATE)
     private Date dataCompra;
 
-    @ManyToOne(targetEntity = Pessoa.class)
+    @ManyToOne(targetEntity = PessoaJuridica.class)
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
-    private Pessoa pessoa;
+    private PessoaJuridica pessoa;
 
     @ManyToOne
     @JoinColumn(name = "conta_pagar_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
     private ContaPagar contaPagar;
 
-    @ManyToOne(targetEntity = Pessoa.class)
+    @ManyToOne(targetEntity = PessoaJuridica.class)
     @JoinColumn(name = "empresa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "empresa_id_fk"))
-    private Pessoa empresa;
+    private PessoaJuridica empresa;
 }
